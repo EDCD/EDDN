a70cfebec9a3d2eba7c23ad70ad86bcd7e4060a1
James Muscat <jamesremuscat@gmail.com>
2014-11-16 11:44:43 +0000

    This should in fact be localhost (or whatever the gateway/announcer is)...

--- src/eddn/conf/Settings.py
+++ src/eddn/conf/Settings.py
@@ -10,7 +10,7 @@ Created on 15 Nov 2014
 # Relay settings
 ###############################################################################
 
-RELAY_RECEIVER_BINDINGS = ["tcp://*:8500"]
+RELAY_RECEIVER_BINDINGS = ["tcp://localhost:8500"]
 
 RELAY_SENDER_BINDINGS = ["tcp://*:9500"]
 
