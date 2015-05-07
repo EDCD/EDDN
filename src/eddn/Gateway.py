69f7134a94f4bca196ed0890a8eb0312ddb3c47e
AnthorNet <anthor.net@gmail.com>
2015-05-07 19:33:59 +0200

    Fix wrong argument in gateway

--- src/eddn/Gateway.py
+++ src/eddn/Gateway.py
@@ -142,7 +142,7 @@ def parse_and_error_handle(data):
 
         # Sends the parsed MarketOrderList or MarketHistoryList to the Announcers
         # as compressed JSON.
-        gevent.spawn(push_message, (simplejson.dumps(parsed_message), parsed_message['$schemaRef']))
+        gevent.spawn(push_message, simplejson.dumps(parsed_message), parsed_message['$schemaRef'])
         logger.info("Accepted %s upload from %s" % (
             parsed_message, get_remote_address()
         ))
