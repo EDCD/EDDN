d9537ac6adcedf34515eb15e57f7436d571adfcf
AlSki <github@alski.net>
2017-05-22 22:41:33 +0100

    Add a .Net example

---
+++ examples/CSharp/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using NetMQ.Sockets;
+using NetMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zlib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var utf8 = new UTF8Encoding();
+
+            using (var client = new SubscriberSocket())
+            {
+                client.Options.ReceiveHighWatermark = 1000;
+                client.Connect("tcp://eddn-relay.elite-markets.net:9500");
+                client.SubscribeToAnyTopic();
+                while (true)
+                {
+                    var bytes = client.ReceiveFrameBytes();
+                    var uncompressed = ZlibStream.UncompressBuffer(bytes);
+
+                    var result = utf8.GetString(uncompressed);
+
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
