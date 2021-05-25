2d2a02265f5493d319b1cdfde59ae02181d51284
BANTYC <BANTYC@DESKTOP>
2021-05-25 23:51:39 +0700

    * Added semicolons * Added ES6 imports * Added package-lock.json * Added module definition at package.json

--- examples/NodeJS/index.js
+++ examples/NodeJS/index.js
@@ -1,19 +1,19 @@
-const zlib = require('zlib')
-const zmq = require('zeromq')
+import zlib from 'zlib';
+import zmq from 'zeromq';
 
-const SOURCE_URL = 'tcp://eddn.edcd.io:9500'
+const SOURCE_URL = 'tcp://eddn.edcd.io:9500';
 
-async function run () {
-  const sock = new zmq.Subscriber
+async function run() {
+  const sock = new zmq.Subscriber;
 
-  sock.connect(SOURCE_URL)
-  sock.subscribe('')
-  console.log('EDDN listener connected to:', SOURCE_URL)
+  sock.connect(SOURCE_URL);
+  sock.subscribe('');
+  console.log('EDDN listener connected to:', SOURCE_URL);
 
-  for await (const [ src ] of sock) {
-    const msg = JSON.parse(zlib.inflateSync(src))
-    console.log(msg)
+  for await (const [src] of sock) {
+    const msg = JSON.parse(zlib.inflateSync(src));
+    console.log(msg);
   }
 }
 
-run()
+run();
