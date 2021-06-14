152bd86818b2ef418a124475072ae8779477ef65
Athanasius <github@miggy.org>
2021-06-14 15:55:49 +0100

    setup.py: A little reformatting for readability

--- setup.py
+++ setup.py
@@ -20,17 +20,27 @@ setup(
     name='eddn',
     version=verstr,
     description='Elite: Dangerous Data Network',
+    long_description="""\
+      The Elite Dangerous Data Network allows ED players to share data. Not affiliated with Frontier Developments.
+      """,
     author='EDCD (https://edcd.github.io/)',
     author_email='edcd@miggy.org',
     url='https://github.com/EDCD/EDDN',
-    packages=find_packages('src', exclude=["*.tests"]),
+
+    packages=find_packages(
+        'src',
+         exclude=["*.tests"]
+    ),
     package_dir = {'':'src'},
+
     # This includes them for the running code, but that doesn't help
     # serve them up for reference.
-    data_files=[('eddn/schemas', glob.glob("schemas/*.json"))],
-    long_description="""\
-      The Elite: Dangerous Data Network allows E:D players to share data. Not affiliated with Frontier Developments.
-      """,
+    data_files=[
+        (
+            'eddn/schemas', glob.glob("schemas/*.json")
+        )
+    ],
+
     # Yes, we pin versions.  With python2.7 the latest pyzmq will NOT
     # work, for instance.
     install_requires=[
@@ -44,11 +54,12 @@ setup(
         "simplejson==3.16.0",
         "mysql-connector-python==8.0.17"
     ],
+
     entry_points={
         'console_scripts': [
             'eddn-gateway = eddn.Gateway:main',
             'eddn-relay = eddn.Relay:main',
             'eddn-monitor = eddn.Monitor:main',
-            ],
-        }
-      )
+        ],
+    }
+)
