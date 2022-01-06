1371f71217286c2cb2f26ea0f7040fa28919922b
Athanasius <github@miggy.org>
2022-01-06 14:06:59 +0000

    Gateway: Set up proper logger formatting
    For some reason the milliseconds portion of the %S timestamp is using a
    comma for decimals separator, despite 'locale' saying we're set to (US)
    English.  /tableflip

--- src/eddn/Gateway.py
+++ src/eddn/Gateway.py
@@ -28,7 +28,14 @@ app = Bottle()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
+__logger_channel = logging.StreamHandler()
+__logger_formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(module)s:%(lineno)d: %(message)s'
+)
+__logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S'
+__logger_formatter.default_msec_format = '%s.%03d'
+__logger_channel.setFormatter(__logger_formatter)
+logger.addHandler(__logger_channel)
 logger.info('Made logger')
 
 
