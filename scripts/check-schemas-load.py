c6e47813bb86d71d423053c46788b56306edc36d
Athanasius <github@miggy.org>
2022-03-12 15:05:10 +0000

    scripts/check-schemas-load: black-formatted, and be verbose

--- scripts/check-schemas-load.py
+++ scripts/check-schemas-load.py
@@ -10,16 +10,16 @@ script_path = pathlib.Path(sys.argv[0])
 root_dir = script_path.parent.parent
 
 # Take every file in the schemas directory
-schemas_dir = root_dir / 'schemas'
+schemas_dir = root_dir / "schemas"
 failures = 0
-for schema_file in schemas_dir.glob('*-v*.*.json'):
-    # print(f'Schema: {schema_file}')
-    with open(schema_file, 'r') as sf:
+for schema_file in schemas_dir.glob("*-v*.*.json"):
+    print(f"Schema: {schema_file}")
+    with open(schema_file, "r") as sf:
         try:
             json = simplejson.load(sf)
 
         except simplejson.JSONDecodeError as e:
-            print(f'Failed to load {schema_file}:\n{e!r}')
+            print(f"Failed to load {schema_file}:\n{e!r}")
             failures += 1
 
 if failures > 0:
