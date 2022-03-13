8c62afb8690f76790291a3f5b3e731ac23e523b3
Athanasius <github@miggy.org>
2022-03-13 15:42:53 +0000

    Make per-component logging more obvious

--- src/eddn/Relay.py
+++ src/eddn/Relay.py
@@ -23,7 +23,7 @@ from zmq import SUBSCRIBE as ZMQ_SUBSCRIBE
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 __logger_channel = logging.StreamHandler()
-__logger_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s:%(lineno)d: %(message)s")
+__logger_formatter = logging.Formatter("%(asctime)s - %(levelname)s - Relay - %(module)s:%(lineno)d: %(message)s")
 __logger_formatter.default_time_format = "%Y-%m-%d %H:%M:%S"
 __logger_formatter.default_msec_format = "%s.%03d"
 __logger_channel.setFormatter(__logger_formatter)
