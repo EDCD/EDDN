bd3fdce53b6f31cdddec00eeaade8590eadfdee1
Athanasius <github@miggy.org>
2022-03-13 16:46:11 +0000

    monitor: Make example eddn-config.js fit the usual live setup

--- contrib/monitor/js/EXAMPLE-eddn-config.js
+++ contrib/monitor/js/EXAMPLE-eddn-config.js
@@ -4,12 +4,11 @@ var eddn_config = {
 	updateInterval: 60000,
 
     eddn_host:          'eddn.edcd.io',
-	monitorEndPoint: 'https://eddn.edcd.io/dev/monitor/',
+	monitorEndPoint:    'https://eddn.edcd.io/monitor/',
 
-	gatewayStats: '/dev/stats/',
+	gatewayStats:       '/stats/',
 
-    relayStats: '/dev/relay/stats/',
-	relayBottlePort: 9110,
+    relayStats:         '/relay/stats/',
 
 	gateways: [
     	'eddn.edcd.io'
@@ -20,3 +19,13 @@ var eddn_config = {
 	] //TODO: Must find a way to bind them to monitor
 
 };
+/* Hard-coded, because even under dev we just use the live URL.  It's
+ * really just for picking out things from the schema URLs, i.e. not used
+ * to direct the browser anywhere.
+ */
+eddn_config['schemasURL'] = 'https://eddn.edcd.io/schemas/';
+/* If you are actually using different URLs for the actual schemas you could
+ * use the following to make it programmatical.
+ */
+// eddn_config['schemasURL'] = 'https://' + eddn_config.eddn_host + '/dev/schemas/';
+
