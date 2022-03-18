5b110fa554f74d32af622a2c157c1c5a64d4e024
Athanasius <github@miggy.org>
2022-03-18 11:56:56 +0000

    Add helper scripts for running from source, and document them.

---
+++ Relay.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Run the EDDN Relay from source
+
+set -e
+
+# Need to be in the src directory, relative to this script
+cd "$(dirname $0)/src" 
+# And we invoke the *module*, not the script by filename.
+python -m eddn.Relay $@
