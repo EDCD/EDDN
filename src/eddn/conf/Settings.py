02c7c9c9607998ca6261f9852f6b048ab5ccd62d
clonedArtie <37144216+clonedArtie@users.noreply.github.com>
2026-06-17 15:03:26 +0200

    Added scanorganic schema to settings

--- src/eddn/conf/Settings.py
+++ src/eddn/conf/Settings.py
@@ -94,7 +94,10 @@ class _Settings(object):
         "https://eddn.edcd.io/schemas/dockinggranted/1"               : "schemas/dockinggranted-v1.0.json",
         "https://eddn.edcd.io/schemas/dockinggranted/1/test"          : "schemas/dockinggranted-v1.0.json",
         "https://eddn.edcd.io/schemas/dockingdenied/1"                : "schemas/dockingdenied-v1.0.json",        
-        "https://eddn.edcd.io/schemas/dockingdenied/1/test"           : "schemas/dockingdenied-v1.0.json",        
+        "https://eddn.edcd.io/schemas/dockingdenied/1/test"           : "schemas/dockingdenied-v1.0.json",   
+
+        "https://eddn.edcd.io/schemas/scanorganic/1"                : "schemas/scanorganic-v1.0.json", 
+        "https://eddn.edcd.io/schemas/scanorganic/1/test"           : "schemas/scanorganic-v1.0.json", 
     }
 
     GATEWAY_OUTDATED_SCHEMAS                = [
